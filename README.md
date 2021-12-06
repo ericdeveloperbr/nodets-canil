522649d33e62ba45decb5437303469c40e15d079
Eric H Marangon <ericdeveloper@outlook.com>
2021-12-06 10:28:04 -0300

    created base

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # nodets-canil
+
+### Pré-requisitps globais
+
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+
+`npm i`
+
+### Para rodar o projeto
+
+`npm run dev`
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+//
+import express from "express";
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+
+// path
+import path from "path";
+
+// routes
+import mainRoutes from './routes/index'
+
+// env
+dotenv.config();
+
+//
+const server = express();
+
+// engine
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+// public
+server.use(express.static(path.join(__dirname, '../public')));
+
+//
+server.use(mainRoutes)
+server.use((req, res) => {
+  res.send('Página não encontrada!')
+})
+
+// server
+server.listen(process.env.PORT)
